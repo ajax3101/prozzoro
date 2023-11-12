c09831f5535010bd16e4af18c8eb23b0916e2a61
Denys Morozov <ajax3101@gmail.com>
2023-11-12 22:14:00 +0200

    make migration

--- zoda/models.py
+++ zoda/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 
+
+class Region(models.Model):
+    class Meta:
+        verbose_name = 'Регіон'
+        verbose_name_plural = 'Регіони'
+        
+    name = models.CharField(max_length=255, verbose_name = 'Назва')
+
+    def __str__(self):
+        return self.name
+
+class Agency(models.Model):
+    class Meta:
+        verbose_name = 'Установа'
+        verbose_name_plural = 'Установи'
+
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name = 'Назва')
+    edrpou = models.CharField(max_length=20, unique=True, verbose_name = 'ЕДРПОУ')
+
+    def __str__(self):
+        return self.name
+
 class Tender(models.Model):
     class Meta:
         verbose_name = 'Тендер'
@@ -15,6 +38,7 @@ class Tender(models.Model):
     t_id = models.CharField(max_length=255, unique=True, verbose_name = 'Ідентифікатор закупівлі')
     cdb = models.CharField(max_length=255, verbose_name = 'Організатор закупівлі')
     t_method = models.CharField(max_length=255, verbose_name = 'Процедура закупівлі')
+ #   edr = models.ForeignKey(Agency, on_delete=models.CASCADE, to_field='edrpou', default='00022504')
 
     
     def __str__(self):
