efb227bf5d00ed1a212f4fcb5053a5b4f1706ee6
Denys Morozov <ajax3101@gmail.com>
2023-11-13 00:01:26 +0200

    add paginator

--- zoda/views.py
+++ zoda/views.py
@@ -1,17 +1,29 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Tender, Region
 
-
-
 def first_page(request):
-    tenders = Tender.objects.all()
+    # Получаем все записи из модели Tender
+    all_tenders = Tender.objects.all()
+
+    # Используем Paginator для разбиения записей на страницы (по 10 записей на страницу)
+    paginator = Paginator(all_tenders, 10)
+
+    # Получаем номер текущей страницы из GET-параметра "page"
+    page = request.GET.get('page')
+
+    # Получаем записи для текущей страницы
+    tenders = paginator.get_page(page)
+
     dict_obj = {
-                'tenders': tenders,
-                }
+        'tenders': tenders,
+    }
+
     return render(request, 'zoda/index.html', dict_obj)
+
 
 class TenderListView(View):
     template_name = 'zoda/tender_list_view.html'
